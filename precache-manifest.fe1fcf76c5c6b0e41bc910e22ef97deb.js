6d9cee94351051df3fec576514917f1e31f08550
Jubo-cade <jubomdivnishvili@gmail.com>
2020-10-14 20:46:29 +0400

    Updates

---
+++ precache-manifest.fe1fcf76c5c6b0e41bc910e22ef97deb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb3abf3dc1f5cc92d211ef99d71a2fc",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "5cb869df853296457c97",
+    "url": "/react-weather-app/static/css/main.0b6fce27.chunk.css"
+  },
+  {
+    "revision": "d661b0d815234a5401ea",
+    "url": "/react-weather-app/static/js/2.44998ee1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.44998ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb869df853296457c97",
+    "url": "/react-weather-app/static/js/main.7390faca.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  },
+  {
+    "revision": "b5138b6d832510d61e33b1faaa2b77b6",
+    "url": "/react-weather-app/static/media/cloudy.b5138b6d.jpg"
+  },
+  {
+    "revision": "9e1b90dd7de38d6eed46e151624cb25b",
+    "url": "/react-weather-app/static/media/sunny.9e1b90dd.jpg"
+  }
+]);
